2b75ea5f1d07b3cad398df7e0091bb9ab0eecbad
kl1900 <63888714+kl1900@users.noreply.github.com>
2022-04-03 18:49:59 -0700

    main frame complete

---
+++ src/client/src/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import UserProfile from "./components/UserProfile";
+import ProductDetail from "./components/ProductDetail";
+import NotFound from "./components/NotFound";
+import SearchResult from "./components/SearchResult";
+import WishList from "./components/WishList";
+
+import "./style/index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/search" element={<SearchResult />} />
+        <Route path="/wishlist/:wishlistId" element={<WishList />} />
+        <Route path="/products/:productId" element={<ProductDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
