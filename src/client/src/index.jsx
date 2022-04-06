8020c72d773fe893413117582740a8eaa6cdd214
kl1900 <63888714+kl1900@users.noreply.github.com>
2022-04-06 16:47:42 -0700

    add applayout to index (#5)

--- src/client/src/index.jsx
+++ src/client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
+import AppLayout from "./components/AppLayout";
 import Login from "./components/Login";
 import UserProfile from "./components/UserProfile";
 import ProductDetail from "./components/ProductDetail";
@@ -17,6 +18,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="app" element={<AppLayout />}></Route>
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/search" element={<SearchResult />} />
