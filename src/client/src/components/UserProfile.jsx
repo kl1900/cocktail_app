35a67a25d5c54341f0505c90902ae4828abbb4ff
kuo lu <kuolu@pm.me>
2022-04-28 14:02:53 -0700

    add ability to edit uer name

--- src/client/src/components/UserProfile.jsx
+++ src/client/src/components/UserProfile.jsx
@@ -1,13 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useAuthToken } from "../AuthTokenContext";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const { user } = useAuth0();
+  const [userName, setUserName] = useState("");
+  const [editMode, setEditMode] = useState(false);
+  const [wishlistCount, setwishlistCount] = useState(0);
+  const [reviewCount, setReviewCount] = useState(0);
+  const { accessToken } = useAuthToken();
+
+  useEffect(() => {
+    async function getUser() {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/me`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = await res.json();
+      if (data) {
+        setUserName(data.name);
+        setwishlistCount(data.wishlist.length);
+        setReviewCount(data.review.length);
+      }
+    }
+
+    if (accessToken) {
+      getUser();
+    }
+  }, [accessToken, editMode]);
+
+  function EditName() {
+    const onSubmit = () => {
+      const data = {
+        newName: document.getElementById("newName").value,
+      };
+      fetch(`${process.env.REACT_APP_API_URL}/me`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data === null) {
+            alert("Duplicate name detected");
+          } else {
+            setUserName(data.name);
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    };
+
+    return (
+      <div>
+        <input id="newName" required placeholder="Enter name" />
+        <button onClick={onSubmit}>Submit</button>
+        <button
+          onClick={() => {
+            setEditMode(false);
+          }}
+        >
+          Cancel
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div className="panel">
       <div>
-        <p>Name: {user.name}</p>
+        <p>Name: {userName}</p>
+        {editMode ? (
+          <div>
+            <EditName />
+          </div>
+        ) : (
+          <button onClick={() => setEditMode(true)}>Edit Name</button>
+        )}
       </div>
       <div>
         <img src={user.picture} width="70" alt="profile avatar" />
@@ -19,7 +95,10 @@ export default function Profile() {
         <p>🔑 Auth0Id: {user.sub}</p>
       </div>
       <div>
-        <p>✅ Email verified: {user.email_verified.toString()}</p>
+        <Link to="/wishlists">my wishlists ({wishlistCount})</Link>
+      </div>
+      <div>
+        <p>total reviews ({reviewCount})</p>
       </div>
     </div>
   );
