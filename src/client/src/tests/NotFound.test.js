b1c997178df9d2a3a05cb80036aa0791c9c460fb
liz <90375135+liz-cs@users.noreply.github.com>
2022-04-30 08:27:43 -0700

    added notfound (#20)

--- src/client/src/tests/NotFound.test.js
+++ src/client/src/tests/NotFound.test.js
@@ -4,8 +4,8 @@ import NotFound from "../components/NotFound";
 import '@testing-library/jest-dom/extend-expect';
 
 test("renders Not Found copy", () => {
-    render( < NotFound /> );
-    expect(screen.getByText("NotFound")).toBeInTheDocument();
+    render( < NotFound / > );
+    expect(screen.getByText("404")).toBeInTheDocument();
 });
 
 // test('mock useState', () => {
